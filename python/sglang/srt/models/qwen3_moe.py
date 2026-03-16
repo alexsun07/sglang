8881410950738d772f4bda84ff116886265ae2dd
Alex Sun <alex.s@amd.com>
2026-03-15 20:59:33 -0500

    support mori ep in qwen

--- python/sglang/srt/models/qwen3_moe.py
+++ python/sglang/srt/models/qwen3_moe.py
@@ -254,7 +254,7 @@ class Qwen3MoeSparseMoeBlock(nn.Module):
             prefix=add_prefix("gate", prefix),
         )
 
-        if get_moe_a2a_backend().is_deepep():
+        if get_moe_a2a_backend().is_deepep() or get_moe_a2a_backend().is_mori():
             # TODO: we will support tp < ep in the future
             self.ep_size = get_moe_expert_parallel_world_size()
             self.num_experts = (
@@ -272,6 +272,7 @@ class Qwen3MoeSparseMoeBlock(nn.Module):
 
         if (
             not get_moe_a2a_backend().is_deepep()
+            and not get_moe_a2a_backend().is_mori()
             and not get_moe_a2a_backend().is_ascend_fuseep()
         ):
             return self.forward_normal(
